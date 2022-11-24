da5b47b1a564b724ae20f462c9d87eae48d607e6
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2022-11-24 23:41:52 +0200

    Minor file reformatting + a few comments

--- Internet/app.js
+++ Internet/app.js
@@ -9,6 +9,7 @@ const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
 
+// Routers
 const loginRouter = require('./routes/login');
 
 // View Engine setup
@@ -31,7 +32,14 @@ db.on('connected', () => {
 // Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+
+// Router bindings
 app.use('/', loginRouter);
+
+
+
+
+
 
 
 app.listen(PORT || 3000, () => console.log(`Server Online. Listening on port ${PORT}`));
