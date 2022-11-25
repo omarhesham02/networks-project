7c72b1d774a1c4f706eb541d68d5875547c73a8d
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2022-11-25 17:33:24 +0200

    Create routers and controllers (#6)
    * Connect to MongoDB using mongoose
    
    * Add check if MongoDB connection is succesful
    
    * Render home page upon login (no user info verification yet)
    
    * Add the first router and controller for GET and POST requests to the homepage
    
    * Minor file reformatting + a few comments
    
    * Removve .env from Gitignore and Add default port values
    
    * Remove .env from gitignore and add default port values

--- Internet/app.js
+++ Internet/app.js
@@ -1,13 +1,16 @@
 // Require dotenv
 require('dotenv').config();
 
-const PORT = process.env.PORT;
-const DB_URI = process.env.DB_URI;
+const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.DB_URI || 'mongodb://0.0.0.0:27017';
 
-var express = require('express');
-var path = require('path');
-var app = express();
-var mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+
+// Routers
+const loginRouter = require('./routes/login');
 
 // View Engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,23 +20,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // Connect to MongoDB using mongoose
 mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection;
 
 // Check if connection is successful
-mongoose.connection.on('connected', () => {
+db.on('connected', () => {
     console.log(`Connected to MongoDB at ${DB_URI}`);
 }); 
 
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.get('/', function(req, res) {
-  res.render('login')
-});
 
-app.post('/', function(req, res) {
-  console.log(req.body);
-  res.render('home')
-});
+// Router bindings
+app.use('/', loginRouter);
+
+
+
 
 app.listen(PORT || 3000, () => console.log(`Server Online. Listening on port ${PORT}`));
 
---
+++ Internet/routes/login.js
@@ -0,0 +1,12 @@
+const loginController = require('../controllers/loginController');
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('login');
+});
+
+router.post('/', loginController.login);
+
+
+
+module.exports = router;
