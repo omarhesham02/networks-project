983b59397f6b217232ae459ddbe8bbc892505167
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2022-11-25 13:17:58 +0200

    Mongodb connection (#3)
    * Connect to MongoDB using mongoose
    
    * Add check if MongoDB connection is succesful
    
    * Render home page upon login (no user info verification yet)

--- Internet/app.js
+++ Internet/app.js
@@ -1,35 +1,39 @@
+// Require dotenv
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const DB_URI = process.env.DB_URI;
+
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
 
-// view engine setup
+// View Engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Connect MongoDB Database
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/express', {useNewUrlParser: true, useUnifiedTopology: true});
+// Connect to MongoDB using mongoose
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-
-// Create MongoDB Database Schema
-var Schema = mongoose.Schema;
-var UserDetail = new Schema({
-    username: String,
-    useremail: String,
-    userpassword: String
-});
+// Check if connection is successful
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB at ${DB_URI}`);
+}); 
 
 
 app.get('/', function(req, res) {
   res.render('login')
 });
 
+app.post('/', function(req, res) {
+  console.log(req.body);
+  res.render('home')
+});
 
-app.listen(3000, () => console.log('Server Online. Listening on port 3000...'));
-
+app.listen(PORT || 3000, () => console.log(`Server Online. Listening on port ${PORT}`));
 
