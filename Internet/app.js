9511b905e9b22931b3a225c6db19df15b77120a7
SaraAlajmy <saraalagami@gmail.com>
2022-11-26 20:49:37 +0200

    Update app.js

--- Internet/app.js
+++ Internet/app.js
@@ -9,13 +9,6 @@ const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
 
-<<<<<<< Updated upstream
-=======
-// Routers
-const loginRouter = require('./routes/login');
-const hikingRouter = require('./routes/hiking'); 
-
->>>>>>> Stashed changes
 // View Engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -41,42 +34,41 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 const loginRouter = require('./routes/login');
 const citiesRouter = require('./routes/cities');
 const islandsRouter = require('./routes/islands');
+const hikingRouter = require('./routes/hiking');
+const incaRouter = require('./routes/inca');
+const parisRouter = require('./routes/paris');
+const searchresultsRouter = require('./routes/searchresults');
+const wanttogoRouter = require('./routes/wanttogo');
+const annapurnaRouter = require('./routes/annapurna');
 //TODO-----
 // const baliRouter = require('./routes/bali');
-// const hikingRouter = require('./routes/hiking');
 // const homeRouter = require('./routes/home');
-// const incaRouter = require('./routes/inca');
-// const parisRouter = require('./routes/paris');
 // const registrationRouter = require('./routes/registration');
 // const romeRouter = require('./routes/rome');
 // const santoriniRouter = require('./routes/santorini');
-// const searchRouter = require('./routes/searchresults');
-// const wanttogoRouter = require('./routes/wanttogo');
+
 //---------
 
 
 
 // Router bindings
 app.use('/', loginRouter);
-<<<<<<< Updated upstream
 app.use('/cities', citiesRouter);
 app.use('/islands', islandsRouter);
+app.use('/hiking', hikingRouter);
+app.use('/inca', incaRouter);
+app.use('/paris', parisRouter);
+app.use('/annapurna', annapurnaRouter);
+app.use('/searchresults', searchresultsRouter);
+app.use('/wanttogo', wanttogoRouter);
 //TODO-----
 // app.use('/bali', baliRouter);
-// app.use('/hiking', hikingRouter);
 // app.use('/home', homeRouter);
-// app.use('/inca', incaRouter);
-// app.use('/paris', parisRouter);
 // app.use('/registration', registrationRouter);
 // app.use('/rome', romeRouter);
 // app.use('/santorini', santoriniRouter);
-// app.use('/searchresults', searchRouter);
-// app.use('/wanttogo', wanttogoRouter);
 //---------
 
-=======
-app.use('/', hikingRouter);
->>>>>>> Stashed changes
 
 
 
