779734d78b9e92689a1f7ae8dcaab62bd6c4e215
MoTammaa <mohamed.abotammaa@student.guc.edu.eg>
2022-12-22 02:21:39 +0200

    handeled if user is signed in, will be redirected

--- Internet/routes/login.js
+++ Internet/routes/login.js
@@ -5,7 +5,10 @@ const MongoClient = app.MongoClient;
 const db = new MongoClient(process.env.DB_URI).db("Users-DB");
 
 router.get('/', (req, res) => {
-    res.render('login');
+    if(req.session.username == undefined)
+        res.render('login');
+    else
+        res.render('home');
 });
 
 router.post('/', loginController.login)
